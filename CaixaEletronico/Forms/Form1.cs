3c2172eca1e28f7f399a4ba1f81744eab82acbf8
Jonas92 <jmferreira-1992@hotmail.com>
2016-07-28 17:45:30 -0300

    Criando namespace para os forms da aplicação

--- CaixaEletronico/Forms/Form1.cs
+++ CaixaEletronico/Forms/Form1.cs
@@ -11,7 +11,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace CaixaEletronico
+namespace CaixaEletronico.Forms
 {
     public partial class Form1 : Form
     {

168ab6392567f70ca78653be00f3dc3fa3d16edf
Jonas92 <jmferreira-1992@hotmail.com>
2016-07-28 10:19:33 -0300

    Selecionando a conta a partir do comboBox

--- CaixaEletronico/Form1.Designer.cs
+++ CaixaEletronico/Form1.Designer.cs
@@ -28,7 +28,6 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
@@ -38,22 +37,13 @@
             this.valorParaOperacaoTxt = new System.Windows.Forms.TextBox();
             this.efetuaDepositoBtn = new System.Windows.Forms.Button();
             this.efetuaSaqueBtn = new System.Windows.Forms.Button();
+            this.comboContas = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(207, 125);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(35, 32);
+            this.label1.Location = new System.Drawing.Point(34, 63);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(44, 13);
             this.label1.TabIndex = 1;
@@ -62,7 +52,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(41, 65);
+            this.label2.Location = new System.Drawing.Point(40, 96);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(38, 13);
             this.label2.TabIndex = 2;
@@ -71,7 +61,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(25, 102);
+            this.label3.Location = new System.Drawing.Point(24, 133);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(54, 13);
             this.label3.TabIndex = 3;
@@ -79,21 +69,21 @@
             // 
             // numeroTxt
             // 
-            this.numeroTxt.Location = new System.Drawing.Point(92, 32);
+            this.numeroTxt.Location = new System.Drawing.Point(91, 63);
             this.numeroTxt.Name = "numeroTxt";
             this.numeroTxt.Size = new System.Drawing.Size(121, 20);
             this.numeroTxt.TabIndex = 4;
             // 
             // titularTxt
             // 
-            this.titularTxt.Location = new System.Drawing.Point(92, 65);
+            this.titularTxt.Location = new System.Drawing.Point(91, 96);
             this.titularTxt.Name = "titularTxt";
             this.titularTxt.Size = new System.Drawing.Size(121, 20);
             this.titularTxt.TabIndex = 5;
             // 
             // SaldoTxt
             // 
-            this.SaldoTxt.Location = new System.Drawing.Point(92, 99);
+            this.SaldoTxt.Location = new System.Drawing.Point(91, 130);
             this.SaldoTxt.Name = "SaldoTxt";
             this.SaldoTxt.Size = new System.Drawing.Size(121, 20);
             this.SaldoTxt.TabIndex = 6;
@@ -125,11 +115,21 @@
             this.efetuaSaqueBtn.UseVisualStyleBackColor = true;
             this.efetuaSaqueBtn.Click += new System.EventHandler(this.efetuaSaqueBtn_Click);
             // 
+            // comboContas
+            // 
+            this.comboContas.FormattingEnabled = true;
+            this.comboContas.Location = new System.Drawing.Point(37, 12);
+            this.comboContas.Name = "comboContas";
+            this.comboContas.Size = new System.Drawing.Size(216, 21);
+            this.comboContas.TabIndex = 10;
+            this.comboContas.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.comboContas);
             this.Controls.Add(this.efetuaSaqueBtn);
             this.Controls.Add(this.efetuaDepositoBtn);
             this.Controls.Add(this.valorParaOperacaoTxt);
@@ -139,7 +139,6 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -149,8 +148,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
@@ -160,6 +157,7 @@
         private System.Windows.Forms.TextBox valorParaOperacaoTxt;
         private System.Windows.Forms.Button efetuaDepositoBtn;
         private System.Windows.Forms.Button efetuaSaqueBtn;
+        private System.Windows.Forms.ComboBox comboContas;
     }
 }
 
--- CaixaEletronico/Form1.cs
+++ CaixaEletronico/Form1.cs
@@ -14,44 +14,44 @@ namespace CaixaEletronico
 {
     public partial class Form1 : Form
     {
-        private Conta conta = new Conta(7921, new Cliente("Zeca Urubu", "650-92"));
+        private Conta[] contas = new Conta[3];
+        private Conta contaSelecionada;
 
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
-            Conta c = new Conta(234, new Cliente("João das Couves", "893849"));
-            c.Deposita(5000);
 
-            Conta cc = new ContaCorrente(123, new Cliente("João das Laranjeiras", "783"));
-            cc.Deposita(2500);
-
-            Conta cp = new ContaPoupanca(789, new Cliente("Maria da Graça", "564356"));
-            cp.Deposita(2500);
-
-            TotalizadorDeContas totalizador = new TotalizadorDeContas();
-            totalizador.Adiciona(c);
-            totalizador.Adiciona(cc);
-            totalizador.Adiciona(cp);
-
-            Banco banco = new Banco();
-            banco.Adiciona(c);
-            banco.Adiciona(cc);
-            banco.Adiciona(cc);
-
-            MessageBox.Show(totalizador.SaldoTotal + "");
-            MessageBox.Show(banco.TotalDeContas() + "");
-
+            CriaContas();
+            AdicionaContasNoCombo();
 
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void AdicionaContasNoCombo()
         {
-            
-            Mostra(conta);
+            foreach (Conta conta in contas)
+                comboContas.Items.Add(conta.Titular.Nome);
+        }
+
+        private void CriaContas()
+        {
+            Conta c, cc, cp;
+
+            c = new Conta(234, new Cliente("João das Couves", "893849"));
+            c.Deposita(5000);
+
+            cc = new ContaCorrente(123, new Cliente("João das Laranjeiras", "783"));
+            cc.Deposita(2500);
+
+            cp = new ContaPoupanca(789, new Cliente("Maria da Graça", "564356"));
+            cp.Deposita(2500);
+
+            contas[0] = c;
+            contas[1] = cc;
+            contas[2] = cp;
         }
 
         private void Mostra(Conta conta)
@@ -63,10 +63,15 @@ namespace CaixaEletronico
 
         private void efetuaDepositoBtn_Click(object sender, EventArgs e)
         {
-            double valor = Convert.ToDouble(valorParaOperacaoTxt.Text);
-            conta.Deposita(valor);
-            Mostra(conta);
-            LimpaValorDeDeposito();
+            if (null == contaSelecionada)
+                MessageBox.Show("Por favor, selecione uma conta antes de realizar qualquer operação!");
+            else
+            {
+                double valor = Convert.ToDouble(valorParaOperacaoTxt.Text);
+                contaSelecionada.Deposita(valor);
+                Mostra(contaSelecionada);
+                LimpaValorDeDeposito();
+            }
         }
 
         private void LimpaValorDeDeposito()
@@ -76,10 +81,22 @@ namespace CaixaEletronico
 
         private void efetuaSaqueBtn_Click(object sender, EventArgs e)
         {
-            double valor = Convert.ToDouble(valorParaOperacaoTxt.Text);
-            conta.Saca(valor);
-            Mostra(conta);
-            LimpaValorDeDeposito();
+            if (null == contaSelecionada)
+                MessageBox.Show("Por favor, selecione uma conta antes de realizar qualquer operação!");
+            else
+            {
+                double valor = Convert.ToDouble(valorParaOperacaoTxt.Text);
+                contaSelecionada.Saca(valor);
+                Mostra(contaSelecionada);
+                LimpaValorDeDeposito();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int indiceDaContaNoCombo = comboContas.SelectedIndex;
+            contaSelecionada = contas[indiceDaContaNoCombo];
+            Mostra(contaSelecionada);
         }
     }
 }
